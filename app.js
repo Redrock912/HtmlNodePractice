c9b34c9f8ceb43a254e0f166633558408eef1739
Redrock <leset912@gmail.com>
2019-02-18 19:54:57 +0900

    day4 bullet and collision(distance)

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ var Entity = function () {
         self.x += self.spdX;
         self.y += self.spdY;
     }
+
+    self.getDistance = function (pt) {
+        return Math.sqrt(Math.pow(self.x - pt.x, 2) + Math.pow(self.y - pt.y, 2));
+    }
     return self;
 }
 
@@ -40,6 +44,8 @@ var Player = function (id) {
     self.pressingLeft = false;
     self.pressingUp = false;
     self.pressingDown = false;
+    self.pressingAttack = false;
+    self.mouseAngle = 0;
     self.maxSpd = 10;
 
     // 무한 루프?
@@ -47,6 +53,16 @@ var Player = function (id) {
     self.update = function () {
         self.updateSpd();
         super_update();
+
+        if (self.pressingAttack) {
+            self.shootBullet(self.mouseAngle);
+        }
+    }
+
+    self.shootBullet = function (angle) {
+        var b = Bullet(self.id, angle);
+        b.x = self.x;
+        b.y = self.y;
     }
 
 
@@ -80,6 +96,10 @@ Player.onConnect = function (socket) {
             player.pressingUp = data.state;
         else if (data.inputId === 'down')
             player.pressingDown = data.state;
+        else if (data.inputId === 'attack')
+            player.pressingAttack = data.state;
+        else if (data.inputId === 'mouseAngle')
+            player.mouseAngle = data.state;
     });
 }
 
@@ -102,12 +122,12 @@ Player.update = function () {
     return pack;
 }
 
-var Bullet = function (angle) {
+var Bullet = function (parent, angle) {
     var self = Entity();
     self.id = Math.random();
     self.spdX = Math.cos(angle / 180 * Math.PI) * 10;
     self.spdY = Math.sin(angle / 180 * Math.PI) * 10;
-
+    self.parent = parent;
     self.timer = 0;
     self.toRemove = false;
 
@@ -116,6 +136,14 @@ var Bullet = function (angle) {
         if (self.timer++ > 100)
             self.toRemove = true;
         super_update();
+
+        for (var i in Player.list) {
+            var p = Player.list[i];
+            if (self.getDistance(p) < 32 && self.parent != p.id) {
+                // handle collision, like hp decreasing
+                self.toRemove = true;
+            }
+        }
     }
 
     Bullet.list[self.id] = self;
@@ -126,15 +154,13 @@ Bullet.list = {};
 
 Bullet.update = function () {
 
-    if (Math.random() < 0.1) {
-        Bullet(Math.random() * 360);
-
-    }
 
     var pack = [];
     for (var i in Bullet.list) {
         var bullet = Bullet.list[i];
         bullet.update();
+        if (bullet.toRemove)
+            delete Bullet.list[i];
         pack.push({
             x: bullet.x,
             y: bullet.y,
